ab237db6bb36e493a12ca9c4df053e8a2e7095ce
Chi Lau <chilau9903@gmail.com>
2024-05-24 17:45:51 +0100

    feat: halfway

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import React from 'react'
+import { Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider,
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage'
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />}/>
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>)
+);
 
 const App = () => {
-  const name = 'John';
-  const x = 10;
-  const y = 20;
-  const names = ['as', 'aa', 'aba', 'aaa']
-  const loggedIn = true;
-  const styles = {
-    color: 'red',
-    fontSize: '55px'
-  }
-  return (
-    <>
-    <div className='text-5xl'>asdf</div>
-    <p style = {styles}>Hello {name}</p>
-    <ul>
-      {names.map((name, index) => (
-        <li key = {index}>{name}</li>
-      ))}
-    </ul>
-    { loggedIn ? <h1>Hello</h1> : <h1>Hello guest</h1>}
-    { loggedIn && <h1>Logged in</h1>}
-    </>
-  )
-}
+  return <RouterProvider router={router} />
+};
 
 export default App
